3a230d495c511027f5718e5abfdbbcdbe3168d6b
Maria <amnaattaria325@gmail.com>
2025-01-25 16:16:19 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,92 @@
+//question 1
+
+// let emptyarr=[
+//     [""],
+//     [""],
+//     [""],
+    
+// ]
+// console.log(emptyarr);
+// //question 2
+// let multidimentionalarray=[
+//     [0,1,2,3],
+//     [1,0,1,2],
+//    [2,1,0,1],
+// ]
+// console.log(multidimentionalarray);
+
+
+//question 3
+
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+
+//question 4
+// console.log('MULTIPLICATION TABLE OF 8');
+
+
+// let table=prompt('Enter a number to show its multiplication table');
+// let length=prompt("Enter length multiplication table");
+// console.log("The length OF table is:",length);
+// for(let i=1;i<=length;i++){
+    
+//     console.log(table +"*"+i + "=" , table*i);
+    
+// }
+
+//question 5
+// let fruits=["apple","banana","mango","orange","strawberry"];
+// for(let i=0;i<fruits.length;i++){
+//     console.log("the element at  index" + i + " is "+fruits[i]);
+
+   
+    
+
+// }
+
+//question 6
+// console.log("counting:\n",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+// console.log("Reverse counting:\n",10,9,8,7,6,5,4,3,2,1,)
+// console.log("Even:\n",0,2,4,6,8,10)
+// console.log("odd\n",1,3,5,7,9,11,13,15,17,19)
+// console.log("series:\n", "2K,4k,6k,8k,10k,12k,14k,16k,18k,20k");
+
+
+//question 7
+// let a=["cake","applepie","chips","cookies","patties"];
+// let userchoice=prompt("welcome to our bakery what do you want to order sir/madam\n cake\napplepie\ncookies\npatties\nchips");
+// for(let i=0;i<=a.length;i++){
+
+//     if(a[i]==userchoice){
+//     console.log("cookie is available at shop")
+// } else{
+//     console.log("we are sorry the thing you want is not avaialable")
+// }
+// }
+
+
+
+//question 8
+
+// let A=[25,53,78,91,12];
+// console.log("Array items\n",A);
+
+// let c=Math.max(25,53,78,91,12);
+// console.log(c);
+
+
+//question 9
+// let num=[25,53,78,91,12];
+// console.log("Array items\n",num);
+// let d=Math.min(25,53,78,91,12);
+// console.log(d);
+
+
+
+//question 10
+
+// for(let i=1;i<=20;i++){
+// console.log(i*5)
+// }
